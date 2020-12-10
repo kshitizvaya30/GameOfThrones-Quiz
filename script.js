f1497b9f61b2ce69e9853e6c23b984462f27b537
kshitizvaya <k.vaya301201@gmail.com>
2020-12-10 19:36:23 +0530

    feat: added play function

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ console.log(chalk.magenta.italic.bold("Now Entering into the world of Game of th
 
 console.log(chalk.cyan.bold("Can you Achieve the Iron Throne or are you destined to  join the Night's Watch along the Wall?"));
 console.log("-----------------")
+
+function play(question , options , answer){
+    console.log(chalk.redBright(question));
+    var userAnswer = readlineSync.question(chalk.yellow(options));
+    
+    if(userAnswer.toUpperCase() === answer.toUpperCase()){
+      console.log(chalk.greenBright("Correct!"));
+      score++;
+    }else{
+      console.log(chalk.greenBright("InCorrect!"));
+      console.log(chalk.greenBright("Sorry , You Have Lost The Throne "));
+      process.exit();
+  
+    }
+    console.log(chalk.greenBright("Current Score : " , score));
+    console.log("---------------");
+  }
