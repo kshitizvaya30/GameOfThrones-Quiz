1dbb1bb116e207049a05d878926a77b7183d2c84
kshitizvaya <k.vaya301201@gmail.com>
2020-12-10 19:38:43 +0530

    feat : added highscore feature

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ console.log(chalk.magenta.italic.bold("Now Entering into the world of Game of th
 console.log(chalk.cyan.bold("Can you Achieve the Iron Throne or are you destined to  join the Night's Watch along the Wall?"));
 console.log("-----------------")
 
+
+
+//DATA OF HIGH SCORES
+var highScores = [
+    {
+      name:"kshitiz",
+      score: 6,
+    },{
+      name:"Priyanshu",
+      score: 5,
+    }
+]
+
+//Play Function
 function play(question , options , answer){
     console.log(chalk.redBright(question));
     var userAnswer = readlineSync.question(chalk.yellow(options));
@@ -50,3 +64,41 @@ function play(question , options , answer){
     options:" A: Death \n B: Blindness \n C: Memory Loss \n D: Uncontrollable Laughter \n",
     answer:"B"
   }];
+
+  //LOOP
+for(var i = 0 ; i < questions.length ;i++){
+    var CurrentQuestion = questions[i];
+    play(CurrentQuestion.question , CurrentQuestion.options, CurrentQuestion.answer);
+  }
+  
+  //FINAL question
+  if(score < 5 ){
+    console.log(chalk.yellowBright.bold("You are not Eligible to sit on the IRON THRONE"));
+  }
+  else{
+    console.log(chalk.yellowBright("Now You have to Answer one Last question To sit on the Throne "));
+    play(chalk.redBright("The Night King was created using a dagger made of:" ),chalk.yellow(" A: Valyrian Steel \n B: Blue Ice \n C: Dragonglass \n D: Weirwood\n"), "C");
+  
+    if(score == 6){
+      console.log(chalk.cyan.bold("Now You Are the King Of 7  Kingdoms"));
+    }
+    else{
+      console.log(chalk.cyan.bold("You have Lost the Battle"));
+    }
+  }
+  
+  
+  //highScores
+  if( score > highScores[1].score ){
+      highScores[1].name = userName;
+      highScores[1].score = score;
+    }
+  
+    
+      console.log(chalk.greenBright("TopScores"));
+      console.log(chalk.yellowBright("kshitiz Vaya - 6"));
+      console.log(chalk.yellowBright(highScores[1].name + "- 6\n"));
+    
+    
+  console.log(chalk.blueBright("IF YOU ARE IN THE TOP 2 , SEND A SCREENSHOT OF YOUR WINNING , SO THAT IT CAN BE UPDATED"));
+  
